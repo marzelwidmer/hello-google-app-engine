c678f001114ce10a78028b9829fa371874c4cf30
Marcel Widmer <marzelwidmer@gmail.com>
2018-05-03 07:22:20 +0200

    add message controller also supporting request params

--- src/main/kotlin/ch/keepcalm/appengine/message/MessageController.kt
+++ src/main/kotlin/ch/keepcalm/appengine/message/MessageController.kt
@@ -2,6 +2,7 @@ package ch.keepcalm.appengine.message
 
 import org.springframework.hateoas.MediaTypes
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 
@@ -9,9 +10,8 @@ import org.springframework.web.bind.annotation.RestController
 class APIController {
 
     @GetMapping(value = arrayOf("/"), produces = arrayOf(MediaTypes.HAL_JSON_UTF8_VALUE))
-     fun message(): Message {
-        return Message("Welcome to the Google AppEngine.")
+     fun message(@RequestParam(value = "message", defaultValue = "Welcome to the Google AppEngine.") message: String): Message {
+        return Message(message)
     }
-
 
 }
